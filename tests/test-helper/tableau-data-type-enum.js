b02dd52d97510611e8d0dc8238436032b2fa7ca3
RobDWaller <rdwaller1984@googlemail.com>
2018-07-16 14:54:02 +0100

    amend to tests helper

--- tests/test-helper/tableau-data-type-enum.js
+++ tests/test-helper/tableau-data-type-enum.js
@@ -1,13 +1,13 @@
 "use strict"
 
 var dataType = {
-    "bool": "bool",
-    "date": "date",
-    "datetime": "datetime",
-    "float": "float",
-    "geometry": "geometry",
-    "int": "int",
-    "string": "string"
+    bool: "bool",
+    date: "date",
+    datetime: "datetime",
+    float: "float",
+    geometry: "geometry",
+    int: "int",
+    string: "string"
 };
 
 export default dataType;
