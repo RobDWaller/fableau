d82aa4d7899334c890bece07addbf6d99447f21d
RobDWaller <rdwaller1984@googlemail.com>
2018-07-16 14:20:34 +0100

    Got first jest test up and running, nightmare.

---
+++ tests/tableau/table.test.js
@@ -0,0 +1,15 @@
+import Table from '../../src/js/tableau/table.js';
+import Pages from '../../src/js/tableau/columns/pages.js'
+import dataType from '../test-helper/tableau-data-type-enum.js';
+
+test('Make a Tableau table object', () => {
+    let pages = new Pages(dataType);
+
+    let table = new Table('pages', 'Facebook Pages', pages);
+
+    let result = table.getTable();
+
+    expect(result.id).toBe('pages');
+    expect(result.alias).toBe('Facebook Pages');
+    expect(result.columns.length).toBe(6);
+});
