c22150eb9d0f8577f042181fdf7557f44c780b85
RobDWaller <rdwaller1984@googlemail.com>
2018-09-02 21:45:46 +0100

    Updated date time tests, make sure they are working

--- tests/helper/date-time.test.js
+++ tests/helper/date-time.test.js
@@ -45,11 +45,11 @@ test('Get tableau time string', () => {
 test('Get tableau time string convert zero', () => {
   let date = new DateTime()
 
-  expect(date.getTableauTimeString('0')).toThrow()
+  expect(() => {date.getTableauTimeString('0')}).toThrow()
 })
 
 test('Get tableau time string convert empty', () => {
   let date = new DateTime()
 
-  expect(date.getTableauTimeString('0')).toThrow()
+  expect(() => {date.getTableauTimeString('')}).toThrow()
 })
